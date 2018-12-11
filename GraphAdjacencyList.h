57e74f3309f8d81230e35c3f795df3ef93ddb83b
StardustJK <1115159016@qq.com>
2018-12-11 13:44:10 +0800

    邻接表实现无向图、有有向图

--- GraphAdjacencyList.h
+++ GraphAdjacencyList.h
@@ -4,19 +4,21 @@
 
 #define MAX_VERTEX_NUM 20 //顶点最大数
 #define VertexType char
+#define InfoType int
 typedef enum { DG, DN, UDG, UDN }GraphKind;//有向图，有向网（有权），无向图，无向网（有权）
 
 //表结点
 typedef struct ArcNode {
 	int adjvex;//该弧指向的顶点的位置
 	struct ArcNode *next;
-	//InfoType *info;
+	InfoType weight;
 }ArcNode;
 
 //头结点
 typedef struct VNode {
 	VertexType data;
 	ArcNode *firstarc;
+	int degree=0;//用作拓扑排序
 }VNode,AdjList[MAX_VERTEX_NUM];
 
 //图结构
@@ -34,7 +36,15 @@ int LocateVex(ALGraph G, VertexType v) {
 	return -1;
 }
 
-bool isExist(ALGraph G,int i,int j) {
+bool isExistDG(ALGraph G, int i, int j) {
+	ArcNode *p = G.vertices[i].firstarc;
+	for (;p != NULL;p = p->next) {
+		if (p->adjvex == j)
+			return 1;
+	}
+	return 0;
+}
+bool isExistUDG(ALGraph G,int i,int j) {
 	ArcNode *p = G.vertices[i].firstarc;
 	for (;p != NULL;p = p->next) {
 		if (p->adjvex == j)
@@ -73,7 +83,7 @@ void createUDG(ALGraph &G) {
 			k--;
 			continue;
 		}
-		if (isExist(G, i, j)) {
+		if (isExistUDG(G, i, j)) {
 			printf("重复输入,请重新输入\n");
 			k--;
 			continue;
@@ -86,12 +96,89 @@ void createUDG(ALGraph &G) {
 	}
 }
 
+
+//有向图
+void createDG(ALGraph &G) {
+	printf("请依次输入顶点个数、弧的条数\n");
+	scanf_s("%d%d", &G.vexnum, &G.arcnum);
+	getchar();
+	printf("请依次输入顶点：\n");//无空格输入
+	for (int i = 0;i < G.vexnum;i++) {
+		scanf_s("%c", &G.vertices[i].data);
+		G.vertices[i].firstarc = NULL;
+	}
+	getchar();
+
+	for (int k = 0;k < G.arcnum;k++) {
+		printf("请按照顶点1 顶点2 的顺序输入第%d组\n", k + 1);
+		int i, j;
+		scanf_s("%c %c", &i, 1, &j, 1);
+		getchar();
+		i = LocateVex(G, i);
+		j = LocateVex(G, j);
+		if (i == -1 || j == -1) {
+			printf("输入错误请重新输入\n");
+			k--;
+			continue;
+		}
+		if (isExistDG(G, i, j)) {
+			printf("重复输入，请重新输入\n");
+			k--;
+			continue;
+		}
+		ArcNode *s;
+		s = (ArcNode*)malloc(sizeof(ArcNode));
+		s->next = G.vertices[i].firstarc;
+		G.vertices[i].firstarc = s;
+		s->adjvex = j;
+		G.vertices[j].degree++;
+	}
+}
+//有向网（带权值
+void createDN(ALGraph &G) {
+	printf("请依次输入顶点个数、弧的条数\n");
+	scanf_s("%d%d", &G.vexnum, &G.arcnum);
+	getchar();
+	printf("请依次输入顶点：\n");//无空格输入
+	for (int i = 0;i < G.vexnum;i++) {
+		scanf_s("%c", &G.vertices[i].data);
+		G.vertices[i].firstarc = NULL;
+	}
+	getchar();
+
+	for (int k = 0;k < G.arcnum;k++) {
+		printf("请按照顶点1 顶点2 权值的格式输入第%d组\n", k + 1);
+		int i, j,w;
+		scanf_s("%c %c %d", &i, 1, &j, 1,&w);
+		getchar();
+		i = LocateVex(G, i);
+		j = LocateVex(G, j);
+		if (i == -1 || j == -1) {
+			printf("输入错误请重新输入\n");
+			k--;
+			continue;
+		}
+		if (isExistDG(G, i, j)) {
+			printf("重复输入，请重新输入\n");
+			k--;
+			continue;
+		}
+		ArcNode *s;
+		s = (ArcNode*)malloc(sizeof(ArcNode));
+		s->next = G.vertices[i].firstarc;
+		G.vertices[i].firstarc = s;
+		s->adjvex = j;
+		s->weight = w;
+		G.vertices[j].degree++;
+	}
+}
+
 void showGraph(ALGraph G) {
 	for (int i = 0;i < G.vexnum;i++) {
 		printf("%c ->", G.vertices[i].data);
 		ArcNode *p = G.vertices[i].firstarc;
 		while(p!=NULL){
-			printf("%d ", p->adjvex);
+			printf("%d ->", p->adjvex);
 			p = p->next;
 		}
 		printf("\n");
